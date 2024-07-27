6b0c3de7fc4b7d8d5dfda565cd87884aa8876e73
Andrea Baccega <vekexasia@gmail.com>
2024-07-27 20:10:16 +0200

    fix: tickets in block extrinsic

--- packages/jam-types/src/JamBlock.ts
+++ packages/jam-types/src/JamBlock.ts
@@ -1,11 +1,18 @@
-import { JamHeader } from "@/index";
+import { JamHeader, RingVRFProof, Tagged } from "@/index";
 
 export interface JamBlock {
   header: JamHeader;
   extrinsics: JamBlockExtrinsics;
 }
 export interface JamBlockExtrinsics {
-  tickets: never[];
+  // Et the maximum number of tickets in a block is
+  // K=16 and it is allowed to be submitted only if current slot is less than Y=500 ( aka lottery did not end yet)
+  // @see section 6.7
+  tickets: Tagged<
+    { entryIndex: 0 | 1; proof: RingVRFProof }[],
+    "block-tickets",
+    { maxLength: 16 }
+  >;
   judgements: never[];
   preimages: never[];
   availability: never[];
