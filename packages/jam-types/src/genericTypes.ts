e42db555611754982af9e8e57b0c385a3f55a1ac
Andrea Baccega <vekexasia@gmail.com>
2024-07-27 21:56:15 +0200

    enh: Added UpToSeq and SeqOfLength
    added 2 utility types to better map the sequences as they're expected in
    jam

--- packages/jam-types/src/genericTypes.ts
+++ packages/jam-types/src/genericTypes.ts
@@ -14,6 +14,7 @@ export type ByteArrayOfLength<T extends number> = Tagged<
   `ByteArrayOfLength${T}`,
   { byteLength: T }
 >;
+
 export type ByteArray32 = ByteArrayOfLength<32>;
 export type ByteArray64 = ByteArrayOfLength<64>;
 export type BandersnatchKey = Tagged<ByteArray32, "BandersnatchKey">;
@@ -24,3 +25,21 @@ export type ED25519PublicKey = Tagged<ByteArray32, "ED25519PublicKey">;
 export type ED25519Signature = Tagged<ByteArray64, "ED25519Signature">;
 export type BLSKey = Tagged<ByteArrayOfLength<144>, "BLSKey">;
 export type RingVRFProof = Tagged<ByteArrayOfLength<784>, "RingVRFProof">;
+
+// Sequences
+/**
+ * Defines type of a sequence having up to L elements
+ */
+export type UpToSeq<
+  T,
+  L extends number,
+  Tag extends string = `UpToSeq${L}`,
+> = Tagged<T[], Tag, { maxLength: L }>;
+/**
+ * Defines type of a sequnce having exactly L elements
+ */
+export type SeqOfLength<
+  T,
+  L extends number,
+  Tag extends string = `UpToSeq${L}`,
+> = Tagged<T[], Tag, { length: L }>;
