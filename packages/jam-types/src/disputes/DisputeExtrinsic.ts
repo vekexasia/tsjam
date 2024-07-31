cdb51788b9f4eb6bad13c7bac569d8954fb39058
Andrea Baccega <vekexasia@gmail.com>
2024-07-31 15:24:31 +0200

    ref: refactor some of the disputes types

--- packages/jam-types/src/disputes/DisputeExtrinsic.ts
+++ packages/jam-types/src/disputes/DisputeExtrinsic.ts
@@ -2,37 +2,59 @@ import {
   ED25519PublicKey,
   ED25519Signature,
   Hash,
+  MinSeqLength,
+  SeqOfLength,
   u32,
 } from "@/genericTypes.js";
-
+import { MINIMUM_VALIDATORS } from "@/consts.js";
+/**
+ * Identified ad E<sub>d</sub> in the paper
+ */
 export interface DisputeExtrinsic {
-  // one ore more verdicts as a compilation of
-  // jusdgements coming from 2/3+1 (current or prevset) active validators
-  verdicts: {
-    /**
-     * the hash of the work report
-     */
-    hash: Hash;
-    /**
-     * Defines which epoch index the validators reporting the verdict are referring to
-     * it can be either current or previous epoch
-     */
-    epochIndex: u32;
-    verdict: Array<{
-      validity: 0 | 1;
+  /**
+   * one ore more verdicts. They must be ordered by .hash
+   */
+  verdicts: MinSeqLength<
+    {
       /**
-       * the index of the validator in the validator set for the specified epoch
+       * the hash of the work report
        */
-      validatorIndex: u32;
+      hash: Hash;
       /**
-       * the signature of the validator
+       * Defines which epoch index the validators reporting the verdict are referring to
+       * it can be either current or previous epoch
        */
-      signature: ED25519Signature;
-    }>;
-  };
+      epochIndex: u32;
 
-  // proofs of misbehaviour of one or more validators to befound invalid
-  // they must be ordered by .ed25519PublicKey
+      /**
+       * the length of this sequence must be at least 2/3+1
+       *
+       * the sequence must be ordered by .validatorIndex
+       */
+      judgements: SeqOfLength<
+        {
+          validity: 0 | 1;
+          /**
+           * the index of the validator in the validator set for the specified epoch
+           */
+          validatorIndex: u32;
+          /**
+           * the signature of the validator
+           */
+          signature: ED25519Signature;
+        },
+        typeof MINIMUM_VALIDATORS // 2/3+1 of 1023 which is the number of validators
+      >;
+    },
+    1
+  >;
+
+  /**
+   * proofs of misbehaviour of one or more validators to befound invalid
+   * they must be ordered by .ed25519PublicKey
+   *
+   * There are 2x entried in the culprit array for ea
+   */
   culprit: Array<{
     /**
      * the hash of the work report
@@ -50,13 +72,30 @@ export interface DisputeExtrinsic {
     signature: ED25519Signature;
   }>;
 
-  // proofs of misbehaviour of one or more validators signing a judgement
-  // in contraddiction with the workreport validity
-  // they must be ordered by .ed25519PublicKey
+  /**
+   * proofs of misbehaviour of one or more validators signing a judgement
+   * in contraddiction with the workreport validity
+   * they must be ordered by .ed25519PublicKey
+   *
+   * There is one entry in the faults array for each verdict containing only valid verdicts matching the workreport hash
+   *
+   */
   faults: Array<{
-    hashe: Hash;
+    /**
+     * the hash of the work report
+     */
+    hash: Hash;
+
+    /**
+     * the signaled validity of the work report
+     */
     validity: 0 | 1;
+
+    /**
+     * the validator public key
+     */
     ed25519PublicKey: ED25519PublicKey;
+
     /**
      * judgement pauload
      */
--- packages/jam-types/src/disputes/DisputesState.ts
+++ packages/jam-types/src/disputes/DisputesState.ts
@@ -3,20 +3,22 @@ import { ED25519PublicKey, Hash } from "@/genericTypes.js";
 /**
  * Section 10 of graypaper
  */
-export interface DisuptesState {
+export interface DisputesState {
   /**
    * the set of hash of work reports
    * that were judged to be **valid**.
    */
   psi_g: Set<Hash>;
+
   /**
    * the set of hash of work reports
-   * that were judged to be **uncertain.**
+   * that were judged to be **bad.**
+   * bad means that the extrinsic had a verdict with 2/3+1 validators saying the validity was 0
    */
   psi_b: Set<Hash>;
+
   /**
-   *
-   set of work reports judged to be wonky or impossible to judge
+   * set of work reports judged to be wonky or impossible to judge
    */
   psi_w: Set<Hash>;
   /**
