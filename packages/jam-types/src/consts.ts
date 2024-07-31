fd879e79752f18ff6823e9a9542b60de0a4f8b68
Andrea Baccega <vekexasia@gmail.com>
2024-07-31 15:24:51 +0200

    wip: add some consts

--- packages/jam-types/src/consts.ts
+++ packages/jam-types/src/consts.ts
@@ -2,3 +2,17 @@
  * Jam Common Era, 1200 UTC on January 1, 2024.
  */
 export const JAM_COMMON_ERA = 1704110400;
+export const NUMBER_OF_VALIDATORS = 1023;
+export const MINIMUM_VALIDATORS = 683;
+export const CORES = 381;
+/**
+ * referred as capital i in the paper
+ */
+export const MAXIMUM_WORK_ITEMS = 4;
+/**
+ * defined in timeslots
+ * @see L in the paper
+ *
+ * it's essentially 1 day => 14400 * 6s = 86400s
+ */
+export const MAXIMUM_AGE_LOOKUP_ANCHOR = 14400;
