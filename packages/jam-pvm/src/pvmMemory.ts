ecf2c3cefec3c45844ad44507b6b940646949673
Andrea Baccega <vekexasia@gmail.com>
2024-08-02 15:57:26 +0200

    feat(pvm): memory should return the exitreason in case access is not allowed

--- packages/jam-pvm/src/pvmMemory.ts
+++ packages/jam-pvm/src/pvmMemory.ts
@@ -1,17 +1,18 @@
 import { u8 } from "@vekexasia/jam-types";
+import { PVMExitReason } from "@/exitReason.js";
 
 const innerMemory = new Uint8Array(0x10000);
 export const PVMMemory = {
-  set(offset: number, value: u8) {
+  set(offset: number, value: u8): PVMExitReason | void {
     innerMemory[offset] = value;
   },
-  setBytes(offset: number, bytes: Uint8Array) {
+  setBytes(offset: number, bytes: Uint8Array): PVMExitReason | void {
     innerMemory.set(bytes, offset);
   },
-  get(offset: number): u8 {
+  get(offset: number): u8 | PVMExitReason {
     return innerMemory[offset] as u8;
   },
-  getBytes(offset: number, length: number) {
+  getBytes(offset: number, length: number): Uint8Array | PVMExitReason {
     return innerMemory.subarray(offset, offset + length);
   },
 };
