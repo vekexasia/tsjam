6c2e174b28d6204b09d3613596c4389acb7d745b
Andrea Baccega <vekexasia@gmail.com>
2024-08-02 16:08:30 +0200

    feat(pvm): 2 reg 1 offset ixs

---
+++ packages/jam-pvm/src/instructions/two_reg_one_offset_ixs.ts
@@ -0,0 +1,108 @@
+import { u32, u8 } from "@vekexasia/jam-types";
+import { GenericPVMInstruction } from "@/instructions/genericInstruction.js";
+import { RegisterIdentifier } from "@/types.js";
+import assert from "node:assert";
+import { readVarIntFromBuffer } from "@/utils/varint.js";
+import { LittleEndian } from "@vekexasia/jam-codec";
+import { Z } from "@/utils/zed.js";
+import { branch } from "@/utils/branch.js";
+
+const create2Reg1OffsetIx = (
+  identifier: u8,
+  name: string,
+  evaluate: GenericPVMInstruction<
+    [rA: RegisterIdentifier, rB: RegisterIdentifier, offset: u32]
+  >["evaluate"],
+): GenericPVMInstruction<[RegisterIdentifier, RegisterIdentifier, u32]> => {
+  return {
+    identifier,
+    name,
+    decode(bytes) {
+      assert(
+        bytes[0] === this.identifier,
+        `invalid identifier expected ${name}`,
+      );
+      const rA = Math.min(12, bytes[1] % 16) as RegisterIdentifier;
+      const rB = Math.min(12, Math.floor(bytes[1] / 16)) as RegisterIdentifier;
+      const lX = Math.min(4, Math.max(0, bytes.length - 2));
+      const offset: u32 = Z(
+        lX,
+        Number(LittleEndian.decode(bytes.subarray(2, 2 + lX))),
+      );
+      return [rA, rB, offset];
+    },
+    evaluate,
+  };
+};
+
+export const branch_eq = create2Reg1OffsetIx(
+  24 as u8,
+  "branch_eq",
+  (context, rA, rB, offset) => {
+    return branch(
+      context,
+      offset,
+      context.registers[rA] === context.registers[rB],
+    );
+  },
+);
+
+export const branch_ne = create2Reg1OffsetIx(
+  30 as u8,
+  "branch_ne",
+  (context, rA, rB, offset) => {
+    return branch(
+      context,
+      offset,
+      context.registers[rA] !== context.registers[rB],
+    );
+  },
+);
+
+export const branch_lt_u = create2Reg1OffsetIx(
+  47 as u8,
+  "branch_lt_u",
+  (context, rA, rB, offset) => {
+    return branch(
+      context,
+      offset,
+      context.registers[rA] < context.registers[rB],
+    );
+  },
+);
+
+export const branch_lt_s = create2Reg1OffsetIx(
+  48 as u8,
+  "branch_lt_s",
+  (context, rA, rB, offset) => {
+    return branch(
+      context,
+      offset,
+      Z(4, context.registers[rA]) < Z(4, context.registers[rB]),
+    );
+  },
+);
+
+export const branch_ge_u = create2Reg1OffsetIx(
+  41 as u8,
+  "branch_ge_u",
+  (context, rA, rB, offset) => {
+    return branch(
+      context,
+      offset,
+      context.registers[rA] >= context.registers[rB],
+    );
+  },
+);
+
+export const branch_ge_s = create2Reg1OffsetIx(
+  43 as u8,
+  "branch_ge_s",
+  (context, rA, rB, offset) => {
+    return branch(
+      context,
+      offset,
+      Z(4, context.registers[rA]) < Z(4, context.registers[rB]),
+    );
+  },
+);
