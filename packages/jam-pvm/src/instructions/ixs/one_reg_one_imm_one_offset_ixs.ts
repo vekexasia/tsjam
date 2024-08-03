20f65c5216b58b04c6f8ab2360136d64e9e8545c
Andrea Baccega <vekexasia@gmail.com>
2024-08-03 22:00:57 +0200

    test(pvm): 1 reg 1 imm 1 offs ixs

--- packages/jam-pvm/src/instructions/ixs/one_reg_one_imm_one_offset_ixs.ts
+++ packages/jam-pvm/src/instructions/ixs/one_reg_one_imm_one_offset_ixs.ts
@@ -6,19 +6,22 @@ import { Z } from "@/utils/zed.js";
 import { readVarIntFromBuffer } from "@/utils/varint.js";
 import { LittleEndian } from "@vekexasia/jam-codec";
 import { regIx } from "@/instructions/ixdb.js";
+import assert from "node:assert";
 
-export const decode1Reg1IMM1Offset = (
+const decode = (
   bytes: Uint8Array,
 ): [register: RegisterIdentifier, vx: u32, offset: u32] => {
-  const ra = Math.min(12, bytes[1] % 16) as RegisterIdentifier;
-  const lx = Math.min(4, Math.floor(bytes[1] / 16) % 8);
-  const ly = Math.min(4, Math.max(0, bytes.length - 2 - lx));
-  const vx = readVarIntFromBuffer(bytes.subarray(2, 2 + lx), lx as u8);
+  assert(bytes.length > 0, "no input bytes");
+  const ra = Math.min(12, bytes[0] % 16) as RegisterIdentifier;
+  const lx = Math.min(4, Math.floor(bytes[0] / 16) % 8);
+  assert(bytes.length >= lx + 1, "not enough bytes");
+  const ly = Math.min(4, Math.max(0, bytes.length - 1 - lx));
+  const vx = readVarIntFromBuffer(bytes.subarray(1, 1 + lx), lx as u8);
   // this is not vy as in the paper since we 're missing the current instruction pointer
   // at this stage. to get vy = ip + offset
   const offset = Z(
     ly,
-    Number(LittleEndian.decode(bytes.subarray(2 + lx, 2 + lx + ly))),
+    Number(LittleEndian.decode(bytes.subarray(1 + lx, 1 + lx + ly)).value),
   ) as u32;
   return [ra, vx, offset];
 };
@@ -34,9 +37,7 @@ const create1Reg1IMM1OffsetIx = (
     identifier: name,
     blockTermination,
     ix: {
-      decode(bytes) {
-        return decode1Reg1IMM1Offset(bytes);
-      },
+      decode,
       evaluate(context, ri, vx, offset) {
         // in reality here offset is i32.
         const vy = (context.instructionPointer + offset) as u32;
@@ -46,7 +47,7 @@ const create1Reg1IMM1OffsetIx = (
   });
 };
 
-export const loadIMMJumpIx = create1Reg1IMM1OffsetIx(
+export const load_imm_jump = create1Reg1IMM1OffsetIx(
   6 as u8,
   "load_imm_jump",
   (context, ri, vx, vy) => {
@@ -56,7 +57,7 @@ export const loadIMMJumpIx = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchEqImm = create1Reg1IMM1OffsetIx(
+export const branch_eq_imm = create1Reg1IMM1OffsetIx(
   7 as u8,
   "branch_eq_imm",
   (context, ri, vx, vy) => {
@@ -65,7 +66,7 @@ export const BranchEqImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchNeImm = create1Reg1IMM1OffsetIx(
+export const branch_ne_imm = create1Reg1IMM1OffsetIx(
   15 as u8,
   "branch_ne_imm",
   (context, ri, vx, vy) => {
@@ -74,7 +75,7 @@ export const BranchNeImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchLTUImm = create1Reg1IMM1OffsetIx(
+export const branch_lt_u_imm = create1Reg1IMM1OffsetIx(
   44 as u8,
   "branch_lt_u_imm",
   (context, ri, vx, vy) => {
@@ -83,7 +84,7 @@ export const BranchLTUImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchLEUImm = create1Reg1IMM1OffsetIx(
+export const branch_le_u_imm = create1Reg1IMM1OffsetIx(
   59 as u8,
   "branch_le_u_imm",
   (context, ri, vx, vy) => {
@@ -92,7 +93,7 @@ export const BranchLEUImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchGEUImm = create1Reg1IMM1OffsetIx(
+export const branch_ge_u_imm = create1Reg1IMM1OffsetIx(
   52 as u8,
   "branch_ge_u_imm",
   (context, ri, vx, vy) => {
@@ -101,7 +102,7 @@ export const BranchGEUImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchGTUImm = create1Reg1IMM1OffsetIx(
+export const branch_gt_u_imm = create1Reg1IMM1OffsetIx(
   50 as u8,
   "branch_gt_u_imm",
   (context, ri, vx, vy) => {
@@ -110,7 +111,7 @@ export const BranchGTUImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchLTSImm = create1Reg1IMM1OffsetIx(
+export const branch_lt_s_imm = create1Reg1IMM1OffsetIx(
   32 as u8,
   "branch_lt_s_imm",
   (context, ri, vx, vy) => {
@@ -119,7 +120,7 @@ export const BranchLTSImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchLESImm = create1Reg1IMM1OffsetIx(
+export const branch_le_s_imm = create1Reg1IMM1OffsetIx(
   46 as u8,
   "branch_le_s_imm",
   (context, ri, vx, vy) => {
@@ -128,7 +129,7 @@ export const BranchLESImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchGESImm = create1Reg1IMM1OffsetIx(
+export const branch_ge_s_imm = create1Reg1IMM1OffsetIx(
   45 as u8,
   "branch_ge_s_imm",
   (context, ri, vx, vy) => {
@@ -137,7 +138,7 @@ export const BranchGESImm = create1Reg1IMM1OffsetIx(
   true,
 );
 
-export const BranchGTSImm = create1Reg1IMM1OffsetIx(
+export const branch_gt_s_imm = create1Reg1IMM1OffsetIx(
   53 as u8,
   "branch_gt_s_imm",
   (context, ri, vx, vy) => {
@@ -145,3 +146,72 @@ export const BranchGTSImm = create1Reg1IMM1OffsetIx(
   },
   true,
 );
+if (import.meta.vitest) {
+  const { describe, expect, it } = import.meta.vitest;
+  type Mock = import("@vitest/spy").Mock;
+  const { createEvContext } = await import("../../../test/mocks.js");
+  describe("decode", () => {
+    const encodeRaLx = (ra: number, lx: number) => {
+      return ra + lx * 16;
+    };
+    it("should decode 1Reg1IMM1Offset", () => {
+      const [ri, vx, offset] = decode(
+        Uint8Array.from([encodeRaLx(1, 1), 0x12, 0x00, 0x00, 0x11]),
+      );
+      expect(ri).toEqual(1);
+      expect(vx).toEqual(0x12000000);
+      expect(offset).toEqual(0x110000);
+    });
+    it("should put 0 in vx if lx is 0", () => {
+      const [ri, vx, offset] = decode(
+        Uint8Array.from([encodeRaLx(1, 0), 0x11]),
+      );
+      expect(ri).toEqual(1);
+      expect(vx).toEqual(0);
+      expect(offset).toEqual(0x11);
+    });
+    it("should max use 4 if lx > 4 and 1 for offset", () => {
+      const [ri, vx, offset] = decode(
+        Uint8Array.from([encodeRaLx(1, 6), 0x12, 0x00, 0x00, 0x11, 1]),
+      );
+      expect(ri).toEqual(1);
+      expect(vx).toEqual(0x12000011);
+      expect(offset).toEqual(1);
+    });
+    it("should discard extra bytes", () => {
+      const [ri, vx, offset] = decode(
+        Uint8Array.from([
+          encodeRaLx(1, 4),
+          0x12,
+          0x00,
+          0x00,
+          0x11,
+          0x00,
+          0x00,
+          0x00,
+          0x00,
+          1, // extra byte
+        ]),
+      );
+      expect(ri).toEqual(1);
+      expect(vx).toEqual(0x12000011);
+      expect(offset).toEqual(0);
+    });
+    it("should decode when lx is 0 and no other bytes", () => {
+      const [ri, vx, offset] = decode(Uint8Array.from([encodeRaLx(1, 0)]));
+      expect(ri).toEqual(1);
+      expect(vx).toEqual(0);
+      expect(offset).toEqual(0);
+    });
+    describe("errors", () => {
+      it("should fail if no input bytes", () => {
+        expect(() => decode(new Uint8Array([]))).toThrow("no input bytes");
+      });
+      it("should fail if not enough bytes", () => {
+        expect(() => decode(new Uint8Array([encodeRaLx(1, 1)]))).toThrow(
+          "not enough bytes",
+        );
+      });
+    });
+  });
+}
