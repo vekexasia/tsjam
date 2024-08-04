d0cb34105c48ca2a2aba61933e1c8c1142a6ff26
Andrea Baccega <vekexasia@gmail.com>
2024-08-04 16:53:34 +0200

    test(pvm): 2 reg 2 imm ixs

--- packages/jam-pvm/src/instructions/ixs/two_reg_two_imm_ixs.ts
+++ packages/jam-pvm/src/instructions/ixs/two_reg_two_imm_ixs.ts
@@ -4,8 +4,27 @@ import { RegisterIdentifier } from "@/types.js";
 import { readVarIntFromBuffer } from "@/utils/varint.js";
 import { djump } from "@/utils/djump.js";
 import { regIx } from "@/instructions/ixdb.js";
+import assert from "node:assert";
 
-const create2Reg2ImmIx = (
+const decode = (
+  bytes: Uint8Array,
+): [rA: RegisterIdentifier, rB: RegisterIdentifier, vx: u32, vy: u32] => {
+  const rA = Math.min(12, bytes[0] % 16) as RegisterIdentifier;
+  const rB = Math.min(12, Math.floor(bytes[0] / 16)) as RegisterIdentifier;
+  assert(bytes.length >= 2, "not enough bytes [1]");
+  const lX = Math.min(4, bytes[1] % 8);
+  const lY = Math.min(4, Math.max(0, bytes.length - 2 - lX));
+  assert(bytes.length >= 2 + lX, "not enough bytes [2]");
+  const vX = readVarIntFromBuffer(bytes.subarray(2, 2 + lX), lX as u8);
+  const vY = readVarIntFromBuffer(
+    bytes.subarray(2 + lX, 2 + lX + lY),
+    lY as u8,
+  );
+
+  return [rA, rB, vX, vY];
+};
+
+const create = (
   identifier: u8,
   name: string,
   evaluate: EvaluateFunction<
@@ -20,28 +39,13 @@ const create2Reg2ImmIx = (
     identifier: name,
     blockTermination,
     ix: {
-      decode(bytes) {
-        const rA = Math.min(12, bytes[1] % 16) as RegisterIdentifier;
-        const rB = Math.min(
-          12,
-          Math.floor(bytes[1] / 16),
-        ) as RegisterIdentifier;
-        const lX = Math.min(4, bytes[2] % 8);
-        const lY = Math.min(4, Math.max(0, bytes.length - 3));
-        const vX = readVarIntFromBuffer(bytes.subarray(3, 3 + lX), lX as u8);
-        const vY = readVarIntFromBuffer(
-          bytes.subarray(3 + lX, 3 + lX + lY),
-          lY as u8,
-        );
-
-        return [rA, rB, vX, vY];
-      },
+      decode,
       evaluate,
     },
   });
 };
 
-export const load_imm_jump_ind = create2Reg2ImmIx(
+export const load_imm_jump_ind = create(
   42 as u8,
   "load_imm_jump_ind",
   (context, rA, rB, vx, vy) => {
@@ -50,3 +54,54 @@ export const load_imm_jump_ind = create2Reg2ImmIx(
   },
   true,
 );
+
+if (import.meta.vitest) {
+  const { vi, describe, expect, it } = import.meta.vitest;
+  vi.mock("@/utils/djump.js", () => ({
+    djump: vi.fn(),
+  }));
+  const { createEvContext } = await import("../../../test/mocks.js");
+  describe("two_reg_two_imm_ixs", () => {
+    describe("decode", () => {
+      it("should fail if not enough bytes", () => {
+        expect(() => decode(new Uint8Array([]))).toThrow(
+          "not enough bytes [1]",
+        );
+        expect(() => decode(new Uint8Array([0]))).toThrow(
+          "not enough bytes [1]",
+        );
+        expect(() => decode(new Uint8Array([0, 1]))).toThrow(
+          "not enough bytes [2]",
+        );
+      });
+      it("should decode rA, rB, vx and vy properly", () => {
+        expect(decode(new Uint8Array([0, 0]))).toEqual([0, 0, 0, 0]);
+        expect(decode(new Uint8Array([1 + 16, 0]))).toEqual([1, 1, 0, 0]);
+        expect(decode(new Uint8Array([13, 0]))).toEqual([12, 0, 0, 0]);
+        expect(decode(new Uint8Array([13 * 16, 0]))).toEqual([0, 12, 0, 0]);
+        expect(decode(new Uint8Array([0, 1, 0x11]))).toEqual([
+          0, 0, 0x11000000, 0,
+        ]);
+        expect(decode(new Uint8Array([1, 0, 0x11]))).toEqual([
+          1, 0, 0, 0x11000000,
+        ]);
+      });
+    });
+    describe("ixs", () => {
+      it("load_imm_jump_ind", () => {
+        const context = createEvContext();
+        context.registers[0] = 0xdeadbeef as u32;
+        context.registers[1] = 0xfffffffe as u32;
+        load_imm_jump_ind.evaluate(
+          context,
+          0 as RegisterIdentifier,
+          1 as RegisterIdentifier,
+          0xdeadbeef as u32,
+          0x00000003 as u32,
+        );
+        expect(djump).toHaveBeenCalledWith(context, 1);
+        expect(context.registers[0]).toBe(0xdeadbeef);
+      });
+    });
+  });
+}
