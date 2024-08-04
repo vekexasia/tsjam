ba6e54302ff17fd0322f768828990fccd3033a8c
Andrea Baccega <vekexasia@gmail.com>
2024-08-04 17:30:37 +0200

    refactor(pvm): no arg ixs

---
+++ packages/jam-pvm/src/instructions/ixs/no_arg_ixs.ts
@@ -0,0 +1,35 @@
+import { u8 } from "@vekexasia/jam-types";
+import { regIx } from "@/instructions/ixdb.js";
+import { RegularPVMExitReason } from "@/exitReason.js";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const fallthrough = regIx<[]>({
+  opCode: 17 as u8,
+  identifier: "fallthrough",
+  blockTermination: true,
+  ix: {
+    decode() {
+      return [];
+    },
+    evaluate() {
+      // TODO: implement this is not specified in the paper. most likely its a useless instruction
+    },
+  },
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const trap = regIx<[]>({
+  opCode: 0 as u8,
+  identifier: "trap",
+  blockTermination: true,
+  ix: {
+    decode() {
+      return [];
+    },
+    evaluate() {
+      return { exitReason: RegularPVMExitReason.Panic };
+    },
+  },
+});
+
+// TODO: implement tests?
