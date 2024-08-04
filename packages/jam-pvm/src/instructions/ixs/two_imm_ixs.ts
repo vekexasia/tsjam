a283466da7a5ad3c2e8976e0bffcb7f46ede0059
Andrea Baccega <vekexasia@gmail.com>
2024-08-04 11:19:17 +0200

    test(pvm): 2 imm ixs

--- packages/jam-pvm/src/instructions/ixs/two_imm_ixs.ts
+++ packages/jam-pvm/src/instructions/ixs/two_imm_ixs.ts
@@ -3,32 +3,34 @@ import { u32, u8 } from "@vekexasia/jam-types";
 import { LittleEndian } from "@vekexasia/jam-codec";
 import { readVarIntFromBuffer } from "@/utils/varint.js";
 import { regIx } from "@/instructions/ixdb.js";
+import assert from "node:assert";
 
 /**
  * decode the full instruction from the bytes.
- * the byte array is chunked to include only the bytes of the instruction (included opcode)
+ * the byte array is chunked to include only the bytes of the instruction
  * @param bytes
  */
-export const decode2IMM = (bytes: Uint8Array): [offset: u32, value: u32] => {
-  let offset = 1;
-  const firstArgLength = Math.min(4, bytes[1]);
+export const decode = (bytes: Uint8Array): [vX: u32, vY: u32] => {
+  let offset = 0;
+  const lX = Math.min(4, bytes[0] % 8);
   offset += 1;
 
+  assert(bytes.length >= offset + lX + (lX == 0 ? 1 : 0), "not enough bytes");
   const first: u32 = readVarIntFromBuffer(
-    bytes.subarray(offset, offset + firstArgLength),
-    firstArgLength as u8,
+    bytes.subarray(offset, offset + lX),
+    lX as u8,
   );
-  offset += firstArgLength;
+  offset += lX;
 
   const secondArgLength = Math.min(4, Math.max(0, bytes.length - offset));
   const second: u32 = readVarIntFromBuffer(
-    bytes.subarray(2 + firstArgLength, 2 + firstArgLength + secondArgLength),
+    bytes.subarray(1 + lX, 1 + lX + secondArgLength),
     secondArgLength as u8,
   );
   return [first, second];
 };
 
-const create2ImmIx = (
+const create = (
   identifier: u8,
   name: string,
   evaluate: EvaluateFunction<[vX: u32, vY: u32]>,
@@ -37,15 +39,13 @@ const create2ImmIx = (
     opCode: identifier,
     identifier: name,
     ix: {
-      decode(bytes) {
-        return decode2IMM(bytes);
-      },
+      decode,
       evaluate,
     },
   });
 };
 
-export const store_imm_u8 = create2ImmIx(
+const store_imm_u8 = create(
   62 as u8,
   "store_imm_u8",
   (context, offset, value) => {
@@ -53,7 +53,7 @@ export const store_imm_u8 = create2ImmIx(
   },
 );
 
-export const store_imm_u16 = create2ImmIx(
+const store_imm_u16 = create(
   79 as u8,
   "store_imm_u16",
   (context, offset, value) => {
@@ -63,7 +63,7 @@ export const store_imm_u16 = create2ImmIx(
   },
 );
 
-export const store_imm_u32 = create2ImmIx(
+const store_imm_u32 = create(
   38 as u8,
   "store_imm_u32",
   (context, offset, value) => {
@@ -72,3 +72,58 @@ export const store_imm_u32 = create2ImmIx(
     context.memory.setBytes(offset, tmp);
   },
 );
+
+if (import.meta.vitest) {
+  const { describe, expect, it } = import.meta.vitest;
+  const { createEvContext } = await import("../../../test/mocks.js");
+  type Mock = import("@vitest/spy").Mock;
+  describe("two_imm_ixs", () => {
+    describe("decode", () => {
+      it("decode just fine", () => {
+        expect(decode(new Uint8Array([0, 0]))).toEqual([0, 0]);
+        expect(decode(new Uint8Array([1, 0x44]))).toEqual([0x44000000, 0]);
+        expect(decode(new Uint8Array([0, 0x44]))).toEqual([0, 0x44000000]);
+        expect(
+          decode(new Uint8Array([1, 0x44, 0x55, 0x11, 0x22, 0x33])),
+        ).toEqual([0x44000000, 0x55112233]);
+        expect(
+          decode(new Uint8Array([4, 0x44, 0x55, 0x11, 0x22, 0x33])),
+        ).toEqual([0x44551122, 0x33000000]);
+        // mod 8 on first param and min 4
+        expect(
+          decode(new Uint8Array([7 + 8, 0x44, 0x55, 0x11, 0x22, 0x33])),
+        ).toEqual([0x44551122, 0x33000000]);
+      });
+      it("should throw if not enough bytes", () => {
+        expect(() => decode(new Uint8Array([0]))).toThrow("not enough bytes");
+        expect(() => decode(new Uint8Array([1]))).toThrow("not enough bytes");
+        expect(() => decode(new Uint8Array([2, 0]))).toThrow(
+          "not enough bytes",
+        );
+      });
+    });
+    describe("ixs", () => {
+      it("store_imm_u8", () => {
+        const context = createEvContext();
+        store_imm_u8.evaluate(context, 0x100 as u32, 0x4422 as u32);
+        expect((context.memory.set as Mock).mock.calls).toEqual([
+          [0x100, 0x22],
+        ]);
+      });
+      it("store_imm_u16", () => {
+        const context = createEvContext();
+        store_imm_u16.evaluate(context, 0x100 as u32, 0x44221133 as u32);
+        expect((context.memory.setBytes as Mock).mock.calls).toEqual([
+          [0x100, new Uint8Array([0x33, 0x11])],
+        ]);
+      });
+      it("store_imm_u32", () => {
+        const context = createEvContext();
+        store_imm_u32.evaluate(context, 0x100 as u32, 0x44221133 as u32);
+        expect((context.memory.setBytes as Mock).mock.calls).toEqual([
+          [0x100, new Uint8Array([0x33, 0x11, 0x22, 0x44])],
+        ]);
+      });
+    });
+  });
+}
