bf62cf58647e6c30db87a78bb853a28db5619933
Andrea Baccega <vekexasia@gmail.com>
2024-08-07 15:10:41 +0200

    fix(extrinsics): buildscript

--- packages/jam-extrinsics/rollup.config.mjs
+++ packages/jam-extrinsics/rollup.config.mjs
@@ -2,11 +2,13 @@ import typescript from '@rollup/plugin-typescript'
 import {rollupCreate, rollupTypes} from "../../build/rollupconfigcreator.mjs";
 
 const tsOptions = {compilerOptions: {rootDir: '.'}};
+
+const external = ['@vekexasia/jam-types', '@vekexasia/jam-codec'];
 /**
  * @type {import('rollup').RollupOptions[]}
  */
 export default [
-  {... rollupCreate({isBrowser: false, isEsm: false}, tsOptions), external: []},
-  {... rollupCreate({isBrowser: false, isEsm: true},tsOptions), external: []},
-  {... rollupTypes(), external: []},
+  {... rollupCreate({isBrowser: false, isEsm: false}, tsOptions), external},
+  {... rollupCreate({isBrowser: false, isEsm: true},tsOptions), external},
+  {... rollupTypes(), external},
 ]
