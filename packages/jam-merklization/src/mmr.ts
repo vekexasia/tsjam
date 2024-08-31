bfd43a8f259ebb0f2cafaf3391c61edb1ffe1c93
Andrea Baccega <vekexasia@gmail.com>
2024-08-31 14:13:03 +0200

    wip(merkle): init merkle impl

---
+++ packages/jam-merklization/src/mmr.ts
@@ -0,0 +1,55 @@
+import { Hash } from "@vekexasia/jam-types";
+import {
+  bigintToBytes,
+  createArrayLengthDiscriminator,
+  JamCodec,
+  OptHashCodec,
+} from "@vekexasia/jam-codec";
+
+/**
+ * section E.2 (301) `A`
+ * @param peeks - the current MMR
+ * @param newPeek - the new element to append
+ * @param hashFn - the hash function
+ */
+export const appendMMR = <T extends Hash>(
+  peeks: Array<T | undefined>,
+  newPeek: T,
+  hashFn: (x: Uint8Array) => T,
+): Array<T | undefined> => {
+  return p(peeks, newPeek, 0, hashFn);
+};
+
+const p = <T extends Hash>(
+  peeks: Array<T | undefined>,
+  newEl: T,
+  pos: number,
+  hashFn: (x: Uint8Array) => T,
+): Array<T | undefined> => {
+  if (pos >= peeks.length) {
+    return peeks.slice().concat(newEl);
+  } else if (pos < peeks.length && peeks[pos] === null) {
+    return replace(peeks, pos, newEl);
+  } else {
+    return p(
+      replace(peeks, pos, undefined),
+      hashFn(
+        new Uint8Array([
+          ...bigintToBytes(peeks[pos]!, 32),
+          ...bigintToBytes(newEl, 32),
+        ]),
+      ),
+      pos + 1,
+      hashFn,
+    );
+  }
+};
+
+const replace = <T>(elements: T[], index: number, value: T) => {
+  const toRet = elements.slice();
+  toRet[index] = value;
+  return toRet;
+};
+
+export const MMRCodec: JamCodec<Array<Hash | undefined>> =
+  createArrayLengthDiscriminator(OptHashCodec);
