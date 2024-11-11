6a5dbd95cd35c72141b6fde77395a5c9392e396d
Andrea Baccega <vekexasia@gmail.com>
2024-11-11 21:15:14 +0100

    refactor: rho

--- packages/jam-transitions/src/rho/toDagger.ts
+++ packages/jam-transitions/src/rho/toDagger.ts
@@ -1,42 +1,52 @@
-import { Dagger, Hash, IDisputesState, Posterior, RHO } from "@tsjam/types";
-import assert from "node:assert";
-import { newSTF, toDagger, toTagged } from "@tsjam/utils";
+import {
+  Dagger,
+  Hash,
+  IDisputesState,
+  Posterior,
+  RHO,
+  STF,
+} from "@tsjam/types";
+import { toDagger, toTagged } from "@tsjam/utils";
+import { err, ok } from "neverthrow";
 
+export enum RHO_2_DaggerError {
+  MISSING_REPORT = "All reports must be present in the rho",
+}
 /**
  * Input should be the newly added hashes to phi_b and phi_w
  * (110)
  *
  */
-export const RHO_2_Dagger = newSTF<RHO, Posterior<IDisputesState>, Dagger<RHO>>(
-  {
-    assertInputValid(input: Posterior<IDisputesState>, curState: RHO) {
-      const hashes = new Set<Hash>(
-        curState
-          .filter((a) => a !== null)
-          .map((a) => {
-            return a!.workReport.workPackageSpecification.workPackageHash;
-          }),
-      );
-      const allPresent = [...input.psi_b, ...input.psi_w].every((a) =>
-        hashes.has(a),
-      );
-      assert(allPresent, "All reports must be present in the rho");
-    },
-    assertPStateValid() {},
-    apply(input: Posterior<IDisputesState>, curState: RHO): Dagger<RHO> {
-      const sets = new Set([...input.psi_b, ...input.psi_w]);
-      const rho_dagger: RHO = toTagged(
-        curState.map((a) => {
-          if (typeof a === "undefined") {
-            return undefined;
-          }
-          if (sets.has(a.workReport.workPackageSpecification.workPackageHash)) {
-            return undefined;
-          }
-          return a;
-        }),
-      );
-      return toDagger(rho_dagger);
-    },
-  },
-);
+export const RHO_2_Dagger: STF<
+  RHO,
+  Posterior<IDisputesState>,
+  RHO_2_DaggerError,
+  Dagger<RHO>
+> = (input, curState) => {
+  const hashes = new Set<Hash>(
+    curState
+      .filter((a) => a !== null)
+      .map((a) => {
+        return a!.workReport.workPackageSpecification.workPackageHash;
+      }),
+  );
+  const allPresent = [...input.psi_b, ...input.psi_w].every((a) =>
+    hashes.has(a),
+  );
+  if (!allPresent) {
+    return err(RHO_2_DaggerError.MISSING_REPORT);
+  }
+  const sets = new Set([...input.psi_b, ...input.psi_w]);
+  const rho_dagger: RHO = toTagged(
+    curState.map((a) => {
+      if (typeof a === "undefined") {
+        return undefined;
+      }
+      if (sets.has(a.workReport.workPackageSpecification.workPackageHash)) {
+        return undefined;
+      }
+      return a;
+    }),
+  );
+  return ok(toDagger(rho_dagger));
+};
--- packages/jam-transitions/src/rho/toDoubleDagger.ts
+++ packages/jam-transitions/src/rho/toDoubleDagger.ts
@@ -7,94 +7,91 @@ import {
   JamState,
   Posterior,
   RHO,
+  STF,
 } from "@tsjam/types";
-import assert from "node:assert";
 import { Ed25519, Hashing } from "@tsjam/crypto";
 import { BitSequence, encodeWithCodec } from "@tsjam/codec";
 import { CORES, JAM_AVAILABLE, NUMBER_OF_VALIDATORS } from "@tsjam/constants";
-import { bigintToBytes, newSTF } from "@tsjam/utils";
+import { bigintToBytes } from "@tsjam/utils";
+import { err, ok } from "neverthrow";
+
+export enum RHO2DoubleDaggerError {
+  EA_LENGTH = "Extrinsic length must be <= than NUMBER_OF_VALIDATORS",
+  VALIDATOR_INDEX = "EA.validatorIndex must be in ascending order",
+  VALIDATOR_INDEX_LESS = "Validator index must be less than NUMBER_OF_VALIDATORS",
+  CORES = "Bitstring length must be CORES",
+  REPORT_PENDING = "Bit may be set if the corresponding core has a report pending availability",
+  SIGNATURE = "EA signature must be valid",
+}
 
 /**
  * converts Dagger<RHO> to DoubleDagger<RHO>
  * (131) & (132) in the greypaper
  */
-export const RHO2DoubleDagger = newSTF<
+export const RHO2DoubleDagger: STF<
   Dagger<RHO>,
   {
     ea: EA_Extrinsic;
     p_kappa: Posterior<JamState["kappa"]>;
     hp: JamHeader["parent"];
   },
+  RHO2DoubleDaggerError,
   DoubleDagger<RHO>
->({
-  assertInputValid(input, curState) {
-    assert(
-      input.ea.length > NUMBER_OF_VALIDATORS,
-      "Extrinsic length must be less than NUMBER_OF_VALIDATORS",
-    );
-    input.ea.reduce((a, b) => {
-      assert(
-        a.validatorIndex < b.validatorIndex,
-        "EA.validatorIndex must be in ascending order",
-      );
-      return b;
-    });
-    input.ea.forEach((a) => {
-      assert(
-        a.validatorIndex < NUMBER_OF_VALIDATORS,
-        "Validator index must be less than NUMBER_OF_VALIDATORS",
-      );
-      assert(a.bitstring.length === CORES, "Bitstring length must be CORES");
-      // (130) in the greypaper
-      for (let i = 0; i < CORES; i++) {
-        if (a.bitstring[i] === 1) {
-          assert(
-            curState[i] === null,
-            "Bit may be set if the corresponding corea has a report pending availaibility",
-          );
+> = (input, curState) => {
+  if (input.ea.length <= NUMBER_OF_VALIDATORS) {
+    return err(RHO2DoubleDaggerError.EA_LENGTH);
+  }
+  for (let i = 1; i < input.ea.length; i++) {
+    if (input.ea[i].validatorIndex < input.ea[i - 1].validatorIndex) {
+      return err(RHO2DoubleDaggerError.VALIDATOR_INDEX);
+    }
+  }
+  for (let i = 0; i < input.ea.length; i++) {
+    const a = input.ea[i];
+    if (a.validatorIndex >= NUMBER_OF_VALIDATORS) {
+      return err(RHO2DoubleDaggerError.VALIDATOR_INDEX_LESS);
+    }
+    if (a.bitstring.length !== CORES) {
+      return err(RHO2DoubleDaggerError.CORES);
+    }
+    // (130) in the greypaper
+    for (let j = 0; j < CORES; j++) {
+      if (a.bitstring[i] === 1) {
+        if (curState[i] !== null) {
+          return err(RHO2DoubleDaggerError.REPORT_PENDING);
         }
       }
-
-      // validate signature (128) in the greypaper
-
-      const encodedBitSequence = encodeWithCodec(BitSequence, a.bitstring);
-      const signatureValid = Ed25519.verifySignature(
-        a.signature,
-        input.p_kappa[a.validatorIndex].ed25519,
-        new Uint8Array([
-          ...JAM_AVAILABLE,
-          ...Hashing.blake2bBuf(
-            new Uint8Array([
-              ...bigintToBytes(input.hp, 32),
-              ...encodedBitSequence,
-            ]),
-          ),
-        ]),
-      );
-      assert(signatureValid, "Signature for EA extrinsic must be valid");
-    });
-  },
-  assertPStateValid() {},
-  apply(
-    input: {
-      ea: EA_Extrinsic;
-      p_kappa: Posterior<JamState["kappa"]>;
-      hp: JamHeader["parent"];
-    },
-    curState: Dagger<RHO>,
-  ) {
-    // (132)
-    const newState = [...curState] as DoubleDagger<RHO>;
-    // todo we could use utilityComputations/availableReports here
-    for (let i = 0; i < CORES; i++) {
-      const availabilitySum = input.ea.reduce(
-        (a: number, b: AssuranceExtrinsic) => a + b.bitstring[i],
-        0,
-      );
-      if (availabilitySum <= (NUMBER_OF_VALIDATORS * 2) / 3) {
-        newState[i] = undefined;
-      }
     }
-    return newState;
-  },
-});
+
+    const encodedBitSequence = encodeWithCodec(BitSequence, a.bitstring);
+    const signatureValid = Ed25519.verifySignature(
+      a.signature,
+      input.p_kappa[a.validatorIndex].ed25519,
+      new Uint8Array([
+        ...JAM_AVAILABLE,
+        ...Hashing.blake2bBuf(
+          new Uint8Array([
+            ...bigintToBytes(input.hp, 32),
+            ...encodedBitSequence,
+          ]),
+        ),
+      ]),
+    );
+    if (!signatureValid) {
+      return err(RHO2DoubleDaggerError.SIGNATURE);
+    }
+  }
+
+  const newState = [...curState] as DoubleDagger<RHO>;
+  // todo we could use utilityComputations/availableReports here
+  for (let i = 0; i < CORES; i++) {
+    const availabilitySum = input.ea.reduce(
+      (a: number, b: AssuranceExtrinsic) => a + b.bitstring[i],
+      0,
+    );
+    if (availabilitySum <= (NUMBER_OF_VALIDATORS * 2) / 3) {
+      newState[i] = undefined;
+    }
+  }
+  return ok(newState);
+};
