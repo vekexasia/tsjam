73e5fe120090c18ed405ceb050909b00502a4943
Andrea Baccega <vekexasia@gmail.com>
2024-12-07 17:44:06 +0100

    assurances test first run

--- packages/jam-codec/test/utils.ts
+++ packages/jam-codec/test/utils.ts
@@ -1,3 +1,14 @@
+import {
+  JamCodec,
+  WorkReportCodec,
+  E_4_int,
+  Optional,
+  BandersnatchCodec,
+  Ed25519PubkeyCodec,
+  IdentityCodec,
+  ValidatorDataCodec,
+} from "@tsjam/codec";
+import { RHO, Tau, ValidatorData, WorkReport } from "@tsjam/types";
 import * as fs from "node:fs";
 
 export const getCodecFixtureFile = (filename: string): Uint8Array => {
@@ -7,4 +18,140 @@ export const getCodecFixtureFile = (filename: string): Uint8Array => {
         .pathname,
     ),
   );
+};
+
+export const TestOutputCodec = <Error extends number, Output>(
+  outputCodec: JamCodec<Output>,
+): JamCodec<{ error?: Error; output?: Output }> => {
+  const toRet: JamCodec<{ error?: Error; output?: Output }> = {
+    encode() {
+      throw new Error("Not implemented");
+    },
+    decode(bytes) {
+      if (bytes[0] === 0) {
+        const output = outputCodec.decode(bytes.subarray(1));
+        return {
+          value: { output: output.value },
+          readBytes: 1 + output.readBytes,
+        };
+      } else {
+        //
+        return {
+          value: { error: bytes[1] as Error },
+          readBytes: 2,
+        };
+      }
+    },
+    encodedSize(value) {
+      return (
+        1 +
+        (typeof value.error !== "undefined"
+          ? 1
+          : outputCodec.encodedSize(value.output!))
+      );
+    },
+  };
+  return toRet;
+};
+export const ValidatorsDataCodec = (nValidators: number) => {
+  const toRet: JamCodec<ValidatorData[]> = {
+    encode(value, bytes) {
+      let offset = 0;
+      for (let i = 0; i < nValidators; i++) {
+        offset += BandersnatchCodec.encode(
+          value[i].banderSnatch,
+          bytes.subarray(offset),
+        );
+        offset += Ed25519PubkeyCodec.encode(
+          value[i].ed25519,
+          bytes.subarray(offset),
+        );
+        offset += IdentityCodec.encode(value[i].blsKey, bytes.subarray(offset));
+        offset += IdentityCodec.encode(
+          value[i].metadata,
+          bytes.subarray(offset),
+        );
+      }
+      return offset;
+    },
+    decode(bytes) {
+      const toRet = [] as unknown as ValidatorData[];
+      let offset = 0;
+      for (let i = 0; i < nValidators; i++) {
+        const d = ValidatorDataCodec.decode(bytes.subarray(offset));
+        toRet.push(d.value);
+        offset += d.readBytes;
+      }
+      return { value: toRet, readBytes: offset };
+    },
+    encodedSize(value) {
+      let size = 0;
+      for (let i = 0; i < nValidators; i++) {
+        size +=
+          BandersnatchCodec.encodedSize(value[i].banderSnatch) +
+          Ed25519PubkeyCodec.encodedSize(value[i].ed25519) +
+          IdentityCodec.encodedSize(value[i].blsKey) +
+          IdentityCodec.encodedSize(value[i].metadata);
+      }
+      return size;
+    },
+  };
+
+  return toRet;
+};
+export const RHOCodec = (cores: number): JamCodec<RHO> => {
+  const opt = new Optional(<
+    JamCodec<{ workReport: WorkReport; reportTime: Tau }>
+  >{
+    encode(value, bytes) {
+      let offset = 0;
+      offset += WorkReportCodec.encode(value.workReport, bytes);
+
+      offset += E_4_int.encode(value.reportTime, bytes.subarray(offset));
+      return offset;
+    },
+    decode(bytes) {
+      let offset = 0;
+      const workReport = WorkReportCodec.decode(bytes.subarray(offset));
+      offset += workReport.readBytes;
+      const reportTime = E_4_int.decode(bytes.subarray(offset, offset + 4));
+      return {
+        value: {
+          workReport: workReport.value,
+          reportTime: <Tau>reportTime.value,
+        },
+        readBytes: offset + 4,
+      };
+    },
+    encodedSize(value) {
+      return WorkReportCodec.encodedSize(value.workReport) + 4;
+    },
+  });
+  const toRet: JamCodec<RHO> = {
+    encode(value, bytes) {
+      let offset = 0;
+      for (let i = 0; i < cores; i++) {
+        offset += opt.encode(value[i], bytes.subarray(offset));
+      }
+      return offset;
+    },
+    decode(bytes) {
+      const toRet = [] as unknown as RHO;
+      let offset = 0;
+      for (let i = 0; i < cores; i++) {
+        const { value, readBytes } = opt.decode(bytes.subarray(offset));
+        offset += readBytes;
+        toRet.push(value);
+      }
+      return { value: toRet, readBytes: offset };
+    },
+    encodedSize(value) {
+      let size = 0;
+      for (let i = 0; i < cores; i++) {
+        size += opt.encodedSize(value[i]);
+      }
+      return size;
+    },
+  };
+  return toRet;
 };
--- packages/jam-core/test/assurances.test.ts
+++ packages/jam-core/test/assurances.test.ts
@@ -1,18 +1,152 @@
-import { describe, expect, it } from "vitest";
-import * as fs from "node:fs";
+import { describe, it, vi, expect } from "vitest";
+
+import { verifyEA } from "@/verifySeal";
 import {
-  assurancesExtrinsicFromJSON,
-  validatorTestCodec,
-  workReportTestCodec,
-} from "@tsjam/codec/test/utils.js";
-import { bigintToBytes, hextToBigInt } from "@tsjam/utils";
+  E_4_int,
+  HashCodec,
+  JamCodec,
+  WorkReportCodec,
+  codec_Ea,
+  createArrayLengthDiscriminator,
+} from "@tsjam/codec";
 import {
-  Dagger,
-  EG_Extrinsic,
-  JamState,
-  RecentHistory,
+  Hash,
+  Tau,
+  EA_Extrinsic,
+  WorkReport,
   RHO,
   Posterior,
+  ValidatorData,
+  JamState,
+  Dagger,
 } from "@tsjam/types";
-import { recentHistoryToPosterior } from "@tsjam/transitions";
-import { verifyEA } from "@/verifySeal";
+import fs from "node:fs";
+import { beforeEach } from "vitest";
+import {
+  TestOutputCodec,
+  ValidatorsDataCodec,
+  RHOCodec,
+} from "@tsjam/codec/test/utils.js";
+import { NUMBER_OF_VALIDATORS } from "@tsjam/constants";
+
+const mocks = vi.hoisted(() => {
+  return {
+    CORES: 2,
+    NUMBER_OF_VALIDATORS: 1,
+  };
+});
+
+vi.mock("@tsjam/constants", async (importOriginal) => {
+  const toRet = {
+    ...(await importOriginal<typeof import("@tsjam/constants")>()),
+    ...mocks,
+  };
+  Object.defineProperty(toRet, "NUMBER_OF_VALIDATORS", {
+    get() {
+      return mocks.NUMBER_OF_VALIDATORS;
+    },
+  });
+  Object.defineProperty(toRet, "CORES", {
+    get() {
+      return mocks.CORES;
+    },
+  });
+  return toRet;
+});
+
+export const getCodecFixtureFile = (
+  filename: string,
+  kind: "tiny" | "full",
+): Uint8Array => {
+  return new Uint8Array(
+    fs.readFileSync(
+      new URL(
+        `../../../jamtestvectors/assurances/${kind}/${filename}`,
+        import.meta.url,
+      ).pathname,
+    ),
+  );
+};
+type TestState = { rho: RHO; p_kappa: ValidatorData[] };
+const testCodec: JamCodec<{
+  input: { ea: EA_Extrinsic; slot: Tau; parent: Hash };
+  output: { error?: 0 | 1 | 2 | 3 | 4 | 5; output?: WorkReport[] };
+  preState: TestState;
+  postState: TestState;
+}> = {
+  encode(value, bytes) {
+    throw new Error("Not implemented");
+  },
+  decode(bytes) {
+    let offset = 0;
+    const ea = codec_Ea.decode(bytes);
+    offset += ea.readBytes;
+    const tau = E_4_int.decode(bytes.subarray(offset, offset + 4));
+    offset += tau.readBytes;
+    const parent = HashCodec.decode(bytes.subarray(offset, offset + 32));
+    // state
+    const rhoCodec = RHOCodec(mocks.CORES);
+    const rho = rhoCodec.decode(bytes.subarray(offset));
+    offset += rho.readBytes;
+    const p_kappa = ValidatorsDataCodec(mocks.NUMBER_OF_VALIDATORS).decode(
+      bytes.subarray(offset),
+    );
+    offset += p_kappa.readBytes;
+
+    const outputCodec = TestOutputCodec<0 | 1 | 2 | 3 | 4 | 5, WorkReport>(
+      createArrayLengthDiscriminator(WorkReportCodec),
+    );
+    const output = outputCodec.decode(bytes.subarray(offset));
+    offset += output.readBytes;
+
+    const postState_rho = rhoCodec.decode(bytes.subarray(offset));
+
+    offset += postState_rho.readBytes;
+    const postState_p_kappa = ValidatorsDataCodec(
+      mocks.NUMBER_OF_VALIDATORS,
+    ).decode(bytes.subarray(offset));
+    offset += postState_p_kappa.readBytes;
+
+    return {
+      value: {
+        input: { ea: ea.value, slot: <Tau>tau.value, parent: parent.value },
+        output: output.value,
+        preState: { rho: rho.value, p_kappa: p_kappa.value },
+        postState: { rho: postState_rho, p_kappa: postState_p_kappa },
+      },
+      readBytes: offset,
+    };
+  },
+  encodedSize(value) {
+    return 0;
+  },
+};
+describe("assurances", () => {
+  const doTest = (filename: string, kind: "tiny" | "full") => {
+    const decoded = testCodec.decode(
+      getCodecFixtureFile(`${filename}.bin`, kind),
+    );
+    return verifyEA(
+      decoded.value.input.ea,
+      decoded.value.input.parent,
+      decoded.value.input.slot,
+      decoded.value.preState.p_kappa as Posterior<JamState["kappa"]>,
+      decoded.value.preState.rho as Dagger<RHO>,
+    );
+  };
+  describe("tiny", () => {
+    beforeEach(() => {
+      mocks.CORES = 2;
+      mocks.NUMBER_OF_VALIDATORS = 6;
+    });
+
+    /*
+    it("no_assurances-1", () => {
+      expect(doTest("no_assurances-1", "tiny")).toBe(true);
+    });
+    */
+    it("some_assurances-1", () => {
+      expect(doTest("some_assurances-1", "tiny")).toBe(true);
+    });
+  });
+});
