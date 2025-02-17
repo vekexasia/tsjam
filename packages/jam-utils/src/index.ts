a3628d1a873a4493a8e75cb4287428fcbcdc1e57
Andrea Baccega <vekexasia@gmail.com>
2025-02-17 20:58:30 +0100

    ref: use neverthrow-safe-ret instead

--- packages/jam-utils/src/index.ts
+++ packages/jam-utils/src/index.ts
@@ -4,22 +4,4 @@ export * from "./historicalLookup.js";
 export * from "./Timekeeping.js";
 export * from "./utils.js";
 export * from "./serviceAccountVirtualElements.js";
-import { Err, Ok } from "neverthrow";
-
-declare module "neverthrow" {
-  export interface Err<T, E> {
-    safeRet(): E extends never ? [E, T] : [E, undefined];
-  }
-
-  export interface Ok<T, E> {
-    safeRet(): T extends never ? [E, T] : [undefined, T];
-  }
-}
-
-Ok.prototype.safeRet = function () {
-  return [undefined, this.value];
-};
-
-Err.prototype.safeRet = function () {
-  return [this.error, undefined];
-};
+import "neverthrow-safe-ret";
