a0914052deaefc7042cadf80f4bea388b8ddd93c
Andrea Baccega <vekexasia@gmail.com>
2025-02-25 14:54:21 +0100

    wip: json codecs with decorators

--- packages/jam-codec/src/json/JsonCodec.ts
+++ packages/jam-codec/src/json/JsonCodec.ts
@@ -1,3 +1,8 @@
+import { HashCodec } from "@/identity";
+import { encodeWithCodec } from "@/utils";
+import type { Hash, WorkPackageHash } from "@tsjam/types";
+import { hextToBigInt } from "@tsjam/utils";
+
 /**
  * Basic utility to convert from/to json
  * mainly used in tests and when debugging is needed
@@ -5,4 +10,113 @@
 export interface JSONCodec<V, J = any> {
   toJSON(value: V): J;
   fromJSON(json: J): V;
+}
+
+// Create a unique symbol for storing codec information
+interface PropertyConfig<T, P extends keyof T = keyof T, V = T[P]> {
+  jsonName: string;
+  property: P;
+  codec: JSONCodec<V, any>;
+}
+
+// Symbol to store metadata
+const jsonPropertiesKey = Symbol("jsonProperties");
+
+// Property decorator factory
+export function JSONProperty<T>(jsonName: string, codec: JSONCodec<T, any>) {
+  return function (target: any, property: string) {
+    const properties = getJsonProperties(target);
+    properties.push({
+      jsonName,
+      codec,
+      property,
+    });
+  };
+}
+
+// Class decorator factory
+export function JSONCodecClass<T extends { new (...args: any[]): any }>(
+  constructor: T,
+) {
+  return class extends constructor implements JSONCodec<T, any> {
+    toJSON(v: T) {
+      const properties = getJsonProperties(v);
+      const result: Record<string, any> = {};
+
+      for (const config of properties) {
+        const value = v[config.property];
+        result[config.jsonName] = config.codec.toJSON(value);
+      }
+      return result;
+    }
+    fromJSON(j: any) {
+      const properties = getJsonProperties(this);
+      for (const { jsonName, property, codec } of properties) {
+        const jsonValue = j[jsonName];
+        if (jsonValue !== undefined) {
+          this[property] = codec.fromJSON(jsonValue);
+        }
+      }
+      return this as unknown as T;
+    }
+  };
+}
+
+// Utility function to get or create property metadata
+function getJsonProperties<T>(target: T): Array<PropertyConfig<T>> {
+  const prototype = Object.getPrototypeOf(target);
+  if (!prototype[jsonPropertiesKey]) {
+    prototype[jsonPropertiesKey] = [];
+  }
+  return prototype[jsonPropertiesKey];
+}
+const bufToHex = (b: Uint8Array) => `0x${Buffer.from(b).toString("hex")}`;
+
+const hashToHex = <T extends Hash>(h: T) =>
+  `${bufToHex(encodeWithCodec(HashCodec, h))}`;
+
+export const HashJSONCodec = <T extends Hash>(): JSONCodec<Hash, string> => {
+  return {
+    toJSON(value) {
+      return hashToHex(value);
+    },
+    fromJSON(json) {
+      return hextToBigInt<T, 32>(json);
+    },
+  };
+};
+export class BaseJSONCodec<V> implements JSONCodec<V, any> {
+  toJSON(value: V) {
+    throw new Error("Decorate class");
+  }
+  fromJSON(json: any): V {
+    throw new Error("Decorate class");
+  }
+}
+
+@JSONCodecClass
+class Person extends BaseJSONCodec<Person> {
+  @JSONProperty("first_name", HashJSONCodec())
+  public firstName!: Hash;
+
+  @JSONProperty("last_name", HashJSONCodec())
+  public lastName!: WorkPackageHash;
+}
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe("decode/encode", () => {
+    it("works", () => {
+      // Usage example
+      const person = new Person();
+      person.lastName = <WorkPackageHash>1n;
+      person.firstName = <Hash>2n;
+
+      const json2 = person.toJSON(person);
+      const restored = person.fromJSON(json2);
+      console.log(json2);
+      console.log(restored);
+    });
+  });
 }
--- packages/jam-codec/src/json/codecs.ts
+++ packages/jam-codec/src/json/codecs.ts
@@ -1,28 +1,187 @@
-import {
+import type {
   AuthorizerPool,
   AuthorizerQueue,
+  AvailabilitySpecification,
+  type BandersnatchKey,
+  Blake2bHash,
+  type BLSKey,
+  type ByteArrayOfLength,
+  CoreIndex,
+  type ED25519PublicKey,
   GammaSFallback,
   GammaSNormal,
+  Gas,
   Hash,
+  type HeaderHash,
   JamEntropy,
   JamState,
-  RecentHistory,
+  type OpaqueHash,
   RecentHistoryItem,
   RHO,
   SafroleState,
+  ServiceIndex,
+  type StateRootHash,
+  Tagged,
   TicketIdentifier,
+  u16,
   ValidatorData,
+  type WorkPackageHash,
 } from "@tsjam/types";
-import { JSONCodec } from "./JsonCodec";
+import {
+  BaseJSONCodec,
+  JSONCodec,
+  JSONCodecClass,
+  JSONProperty,
+} from "./JsonCodec";
 import { hexToBytes, hextToBigInt, isFallbackMode } from "@tsjam/utils";
 import { encodeWithCodec } from "@/utils";
-import { HashCodec } from "@/identity";
+import { Ed25519PubkeyCodec, HashCodec } from "@/identity";
 import { ValidatorDataCodec } from "@/validatorDataCodec";
+import assert from "node:assert";
+import { MAXIMUM_AGE_LOOKUP_ANCHOR } from "@tsjam/constants";
 
 const bufToHex = (b: Uint8Array) => `0x${Buffer.from(b).toString("hex")}`;
 
 const hashToHex = <T extends Hash>(h: T) =>
   `${bufToHex(encodeWithCodec(HashCodec, h))}`;
+
+export const BigIntJSONCodec = <T extends bigint>(): JSONCodec<T, number> => {
+  return {
+    fromJSON(json) {
+      return <T>BigInt(json);
+    },
+    toJSON(value) {
+      return Number(value); // TODO: this might fail due to loss in precision
+    },
+  };
+};
+
+export const NumberJSONCodec = <T extends number>(): JSONCodec<T, number> => {
+  return {
+    fromJSON(json) {
+      return <T>json;
+    },
+    toJSON(value) {
+      return value;
+    },
+  };
+};
+
+export const HashJSONCodec = <T extends Hash>(): JSONCodec<Hash, string> => {
+  return {
+    toJSON(value) {
+      return hashToHex(value);
+    },
+    fromJSON(json) {
+      return hextToBigInt<T, 32>(json);
+    },
+  };
+};
+
+export const Ed25519JSONCodec = (): JSONCodec<ED25519PublicKey, string> => {
+  return {
+    toJSON(value) {
+      return bufToHex(encodeWithCodec(Ed25519PubkeyCodec, value));
+    },
+    fromJSON(json) {
+      return hextToBigInt<ED25519PublicKey, 32>(json);
+    },
+  };
+};
+
+export const BufferJSONCodec = <
+  T extends ByteArrayOfLength<K>,
+  K extends number,
+>(): JSONCodec<T, string> => {
+  return {
+    fromJSON(json) {
+      return hexToBytes(json);
+    },
+    toJSON(value) {
+      return bufToHex(value);
+    },
+  };
+};
+
+export const ArrayOfJSONCodec = <K extends T[], T>(
+  singleCodec: JSONCodec<T>,
+): JSONCodec<K, any[]> => {
+  return {
+    fromJSON(json) {
+      return <K>json.map((item) => singleCodec.fromJSON(item));
+    },
+    toJSON(value) {
+      return value.map((item) => singleCodec.toJSON(item));
+    },
+  };
+};
+
+export const MapJSONCoec = <K, V, KN extends string, VN extends string>(
+  jsonKeys: {
+    key: KN;
+    value: VN;
+  },
+  keyCodec: JSONCodec<K, any>,
+  valueCodec: JSONCodec<V, any>,
+): JSONCodec<Map<K, V>, Array<{ [key in KN | VN]: any }>> => {
+  return {
+    fromJSON(json) {
+      return new Map<K, V>(
+        json.map((item) => [
+          keyCodec.fromJSON(item[jsonKeys.key]),
+          valueCodec.fromJSON(item[jsonKeys.value]),
+        ]),
+      );
+    },
+    toJSON(value) {
+      return <any>[...value.entries()].map(([key, value]) => ({
+        [jsonKeys.key]: keyCodec.toJSON(key),
+        [jsonKeys.value]: valueCodec.toJSON(value),
+      }));
+    },
+  };
+};
+
+export const WrapJSONCodec = <T, K extends string>(
+  key: K,
+  codec: JSONCodec<T, any>,
+): JSONCodec<T, { [key in K]: any }> => {
+  return {
+    fromJSON(json) {
+      return codec.fromJSON(json[key]);
+    },
+    toJSON(value) {
+      return <{ [key in K]: any }>{
+        [key]: codec.toJSON(value),
+      };
+    },
+  };
+};
+
+export const NULLORCodec = <T>(
+  tCodec: JSONCodec<T, unknown>,
+): JSONCodec<T | undefined, unknown | null> => {
+  return {
+    fromJSON(json) {
+      if (json === null) {
+        return undefined;
+      }
+      return tCodec.fromJSON(json);
+    },
+    toJSON(value) {
+      if (typeof value === "undefined") {
+        return null;
+      }
+      return tCodec.toJSON(value);
+    },
+  };
+};
+
+// util to extract types from jsoncodec
+type ExtractNativeType<T> =
+  T extends JSONCodec<infer First, any> ? First : never;
+type ExtractJSONType<T> =
+  T extends JSONCodec<any, infer Second> ? Second : never;
 
 export const AuthPoolJsonCodec: JSONCodec<AuthorizerPool> = {
   fromJSON(json: string[][]) {
@@ -52,108 +211,57 @@ export const AuthQueueJsonCodec: JSONCodec<AuthorizerQueue> = {
   },
 };
 
-export const RecentHistoryCodec: JSONCodec<
-  RecentHistory,
-  Array<{
-    header_hash: string;
-    mmr: { peaks: Array<null | string> };
-    state_root: string;
-    reported: Array<{ work_package_hash: string; segment_tree_root: string }>;
-  }>
-> = {
-  fromJSON(json) {
-    return <RecentHistory>json.map((item) => {
-      return <RecentHistoryItem>{
-        headerHash: hextToBigInt(item.header_hash),
-        accumulationResultMMR: item.mmr.peaks.map((item) => {
-          if (item == null) {
-            return undefined;
-          } else {
-            return hextToBigInt(item);
-          }
-        }),
-        stateRoot: hextToBigInt(item.state_root),
-        reportedPackages: new Map(
-          item.reported.map((a) => [
-            hextToBigInt(a.work_package_hash),
-            hextToBigInt(a.segment_tree_root),
-          ]),
-        ),
-      };
-    });
-  },
+@JSONCodecClass
+export class RecentHistoryItemJSON
+  extends BaseJSONCodec<RecentHistoryItemJSON>
+  implements RecentHistoryItem
+{
+  @JSONProperty("header_hash", HashJSONCodec())
+  headerHash!: HeaderHash;
 
-  toJSON(value) {
-    return value.map((item) => {
-      return {
-        header_hash: hashToHex(item.headerHash),
-        mmr: {
-          peaks: item.accumulationResultMMR.map((item) => {
-            if (typeof item === "undefined") {
-              return null;
-            }
-            return hashToHex(item);
-          }),
-        },
-        state_root: hashToHex(item.stateRoot),
-        reported: [...item.reportedPackages.entries()].map(([wph, str]) => ({
-          work_package_hash: hashToHex(wph),
-          segment_tree_root: hashToHex(str),
-        })),
-      };
-    });
-  },
-};
+  @JSONProperty(
+    "mmr",
+    WrapJSONCodec("peaks", ArrayOfJSONCodec(NULLORCodec(HashJSONCodec()))),
+  )
+  accumulationResultMMR!: (Hash | undefined)[];
 
-const ValidatorDataJSONCodec: JSONCodec<
-  Array<ValidatorData>,
-  Array<{
-    bandersnatch: string;
-    ed25519: string;
-    bls: string;
-    metadata: string;
-  }>
-> = {
-  fromJSON(json) {
-    return json.map((item) => {
-      return <ValidatorData>{
-        banderSnatch: hexToBytes(item.bandersnatch),
-        ed25519: hextToBigInt(item.ed25519),
-        metadata: hexToBytes(item.metadata),
-        blsKey: hexToBytes(item.bls),
-      };
-    });
-  },
+  @JSONProperty("state_root", HashJSONCodec())
+  stateRoot!: StateRootHash;
 
-  toJSON(value) {
-    return value.map((item) => {
-      const b = encodeWithCodec(ValidatorDataCodec, item);
-      return {
-        bandersnatch: bufToHex(b.subarray(0, 32)),
-        ed25519: bufToHex(b.subarray(32, 64)),
-        metadata: bufToHex(b.subarray(64, 64 + 144)),
-        bls: bufToHex(b.subarray(64 + 144)),
-      };
-    });
-  },
-};
-const buildValidatorDataJSONCodec = <T extends Array<ValidatorData>>() => {
-  return <
-    JSONCodec<
-      T,
-      Array<{
-        bandersnatch: string;
-        ed25519: string;
-        bls: string;
-        metadata: string;
-      }>
-    >
-  >ValidatorDataJSONCodec;
-};
+  @JSONProperty(
+    "reported",
+    MapJSONCoec(
+      { key: "work_package_hash", value: "segment_tree_root" },
+      HashJSONCodec(),
+      HashJSONCodec(),
+    ),
+  )
+  reportedPackages!: Map<WorkPackageHash, Hash>;
+}
+export const RecentHistoryCodec = ArrayOfJSONCodec(new RecentHistoryItemJSON());
 
-export const GammaKJsonCodec = buildValidatorDataJSONCodec<
-  SafroleState["gamma_k"]
->;
+@JSONCodecClass
+export class ValidatorDataJSON
+  extends BaseJSONCodec<ValidatorDataJSON>
+  implements ValidatorData
+{
+  @JSONProperty("bandersnatch", BufferJSONCodec())
+  banderSnatch!: BandersnatchKey;
+
+  @JSONProperty("ed25519", Ed25519JSONCodec())
+  ed25519!: ED25519PublicKey;
+
+  @JSONProperty("bls", BufferJSONCodec())
+  blsKey!: BLSKey;
+
+  @JSONProperty("metadata", BufferJSONCodec())
+  metadata!: ByteArrayOfLength<128>;
+}
+
+export const GammaKJsonCodec = ArrayOfJSONCodec<
+  SafroleState["gamma_k"],
+  ValidatorData
+>(new ValidatorDataJSON());
 
 export const GammaSJsonCodec: JSONCodec<
   SafroleState["gamma_s"],
@@ -188,55 +296,100 @@ export const GammaSJsonCodec: JSONCodec<
   },
 };
 
-export const GammaAJsonCodec: JSONCodec<
-  SafroleState["gamma_a"],
-  { tickets: Array<{ id: string; attempt: number }> }
-> = {
-  fromJSON(json) {
-    // means tickets are there
-    return <SafroleState["gamma_a"]>json.tickets.map((ticket) => {
-      const t: TicketIdentifier = {
-        id: hextToBigInt(ticket.id),
-        attempt: <0 | 1>ticket.attempt,
-      };
-      return t;
-    });
-  },
+@JSONCodecClass
+export class TicketIdentifierJSON
+  extends BaseJSONCodec<TicketIdentifierJSON>
+  implements TicketIdentifier
+{
+  @JSONProperty("id", HashJSONCodec())
+  public id!: OpaqueHash;
+  @JSONProperty("attempt", NumberJSONCodec())
+  public attempt!: 0 | 1;
+}
 
-  toJSON(value) {
-    return {
-      tickets: value.map((v) => ({
-        id: hashToHex(v.id),
-        attempt: v.attempt,
-      })),
-    };
-  },
-};
+export const GammaAJsonCodec = WrapJSONCodec(
+  "tickets",
+  ArrayOfJSONCodec<SafroleState["gamma_a"], TicketIdentifier>(
+    new TicketIdentifierJSON(),
+  ),
+);
 
 //TODO: psi/DisputesState
 
-export const EntropyJSONCodec: JSONCodec<JamEntropy, string[]> = {
-  fromJSON(json) {
-    return <JamEntropy>json.map((a) => hextToBigInt(a));
-  },
-  toJSON(value) {
-    return value.map((a) => hashToHex(a));
-  },
-};
+export const EntropyJSONCodec = ArrayOfJSONCodec<JamEntropy, Blake2bHash>(
+  BigIntJSONCodec(),
+);
 
-export const IOTAJSONCodec = buildValidatorDataJSONCodec<JamState["iota"]>();
-export const KappaJSONCodec = buildValidatorDataJSONCodec<JamState["kappa"]>();
-export const LambdaJSONCodec =
-  buildValidatorDataJSONCodec<JamState["lambda"]>();
+export const IOTAJSONCodec = ArrayOfJSONCodec<JamState["iota"], ValidatorData>(
+  new ValidatorDataJSON(),
+);
 
-type JSONWorkReport = {
-  package_spec: {
+export const KappaJSONCodec = ArrayOfJSONCodec<
+  JamState["kappa"],
+  ValidatorData
+>(new ValidatorDataJSON());
+
+export const LambdaJSONCodec = ArrayOfJSONCodec<
+  JamState["lambda"],
+  ValidatorData
+>(new ValidatorDataJSON());
+
+@JSONCodecClass
+export class AvailabilitySpecificationJSON
+  extends BaseJSONCodec<AvailabilitySpecificationJSON>
+  implements AvailabilitySpecification
+{
+  @JSONProperty("hash", HashJSONCodec())
+  public workPackageHash!: WorkPackageHash;
+
+  @JSONProperty("length", NumberJSONCodec())
+  public bundleLength!: Tagged<
+    number,
+    "l",
+    { maxValue: typeof MAXIMUM_AGE_LOOKUP_ANCHOR }
+  >;
+
+  @JSONProperty("erasure_root", HashJSONCodec())
+  public erasureRoot!: Hash;
+
+  @JSONProperty("exports_root", HashJSONCodec())
+  public segmentRoot!: Hash;
+
+  @JSONProperty("exports_count", NumberJSONCodec())
+  public segmentCount!: u16;
+}
+
+export const AvailabilitySpecJSONCodec: JSONCodec<
+  AvailabilitySpecification,
+  {
     hash: string;
     length: number;
     erasure_root: string;
     exports_root: string;
     exports_count: number;
-  };
+  }
+> = {
+  fromJSON(json) {
+    return {
+      workPackageHash: hextToBigInt(json.hash),
+      bundleLength: <AvailabilitySpecification["bundleLength"]>json.length,
+      erasureRoot: hextToBigInt(json.erasure_root),
+      segmentRoot: hextToBigInt(json.exports_root),
+      segmentCount: <u16>json.exports_count,
+    };
+  },
+  toJSON(value) {
+    return {
+      hash: hashToHex(value.workPackageHash),
+      length: value.bundleLength,
+      erasure_root: hashToHex(value.erasureRoot),
+      exports_root: hashToHex(value.segmentRoot),
+      exports_count: value.segmentCount,
+    };
+  },
+};
+type JSONWorkReport = {
+  package_spec: ExtractJSONType<typeof AvailabilitySpecJSONCodec>;
   context: {
     anchor: string;
     state_root: string;
@@ -248,7 +401,10 @@ type JSONWorkReport = {
   core_index: number;
   authorizer_hash: string;
   auth_output: string;
-  segment_root_lookup: string[];
+  segment_root_lookup: Array<{
+    work_package_hash: string;
+    segment_tree_root: string;
+  }>;
   results: Array<{
     service_id: number;
     code_hash: string;
@@ -271,20 +427,9 @@ export const RHOJSONCodec: JSONCodec<
       }
       return {
         report: {
-          package_spec: {
-            hash: hashToHex(
-              item.workReport.workPackageSpecification.workPackageHash,
-            ),
-            length: item.workReport.workPackageSpecification.bundleLength,
-            erasure_root: hashToHex(
-              item.workReport.workPackageSpecification.erasureRoot,
-            ),
-            exports_root: hashToHex(
-              item.workReport.workPackageSpecification.segmentRoot,
-            ),
-            exports_count:
-              item.workReport.workPackageSpecification.segmentCount,
-          },
+          package_spec: AvailabilitySpecJSONCodec.toJSON(
+            item.workReport.workPackageSpecification,
+          ),
           context: {
             anchor: hashToHex(item.workReport.refinementContext.anchor.hash),
             state_root: hashToHex(
@@ -304,12 +449,74 @@ export const RHOJSONCodec: JSONCodec<
           },
           core_index: item.workReport.coreIndex,
           authorizer_hash: hashToHex(item.workReport.authorizerHash),
+          auth_output: bufToHex(item.workReport.authorizerOutput),
+          segment_root_lookup: [
+            ...item.workReport.segmentRootLookup.entries(),
+          ].map(([wph, str]) => ({
+            work_package_hash: hashToHex(wph),
+            segment_tree_root: hashToHex(str),
+          })),
+          results: item.workReport.results.map((res) => {
+            return {
+              service_id: res.serviceIndex,
+              code_hash: hashToHex(res.codeHash),
+              payload_hash: hashToHex(res.payloadHash),
+              accumulate_gas: Number(res.gasPrioritization),
+              result: (() => {
+                if (res.output instanceof Uint8Array) {
+                  return { ok: bufToHex(res.output) };
+                } else {
+                  // FIXME: missing
+                  return { ok: "" };
+                }
+              })(),
+            };
+          }),
         },
         timeout: item.reportTime,
       };
     });
   },
-  fromJSON(json) {},
+  fromJSON(json) {
+    return json.map((item) => {
+      if (item === null) {
+        return undefined;
+      }
+      return {
+        reportTime: item.timeout,
+        workReport: {
+          workPackageSpecification: AvailabilitySpecJSONCodec.fromJSON(
+            item.report.package_spec,
+          ),
+          refinementContext: {},
+          coreIndex: <CoreIndex>item.report.core_index,
+          authorizerHash: hextToBigInt(item.report.authorizer_hash),
+          authorizerOutput: hexToBytes(item.report.auth_output),
+          segmentRootLookup: new Map(
+            item.report.segment_root_lookup.map((a) => [
+              hextToBigInt(a.work_package_hash),
+              hextToBigInt(a.segment_tree_root),
+            ]),
+          ),
+          results: item.report.results.map((res) => {
+            return {
+              serviceIndex: <ServiceIndex>res.service_id,
+              codeHash: hextToBigInt(res.code_hash),
+              payloadHash: hextToBigInt(res.payload_hash),
+              gasPrioritization: <Gas>BigInt(res.accumulate_gas),
+              output: (() => {
+                assert(
+                  typeof res.result.ok !== "undefined",
+                  "result not ok not implemented",
+                );
+                return hexToBytes(res.result.ok);
+              })(),
+            };
+          }),
+        },
+      };
+    });
+  },
 };
 
 if (import.meta.vitest) {
