f0a384de814163864314076af9b9ab3e81eccaf5
Andrea Baccega <vekexasia@gmail.com>
2025-03-06 09:45:47 +0100

    feat: et extrinsic json codec

--- packages/jam-codec/src/extrinsics/tickets.ts
+++ packages/jam-codec/src/extrinsics/tickets.ts
@@ -1,5 +1,11 @@
 import { RingVRFProof, TicketExtrinsics } from "@tsjam/types";
 import { createArrayLengthDiscriminator } from "@/lengthdiscriminated/arrayLengthDiscriminator.js";
+import {
+  ArrayOfJSONCodec,
+  BufferJSONCodec,
+  createJSONCodec,
+  NumberJSONCodec,
+} from "@/json/JsonCodec";
 
 /**
  * $(0.6.1 - C.14)
@@ -30,20 +36,41 @@ export const codec_Et = createArrayLengthDiscriminator<TicketExtrinsics>({
   },
 });
 
+export const codec_Et_JSON = ArrayOfJSONCodec<
+  TicketExtrinsics,
+  TicketExtrinsics[0],
+  { attempt: number; signature: string }
+>(
+  createJSONCodec([
+    ["entryIndex", "attempt", NumberJSONCodec<0 | 1>()],
+    ["proof", "signature", BufferJSONCodec<RingVRFProof, 784>()],
+  ]),
+);
+
 if (import.meta.vitest) {
   const { describe, expect, it } = import.meta.vitest;
 
   const { getCodecFixtureFile } = await import("@/test/utils.js");
   const { encodeWithCodec } = await import("@/utils");
   describe("codecEt", () => {
-    const bin = getCodecFixtureFile("tickets_extrinsic.bin");
-    it("tickets_extrinsic.json encoded should match tickets_extrinsic.bin", () => {
+    it("tickets_extrinsic.bin", () => {
+      const bin = getCodecFixtureFile("tickets_extrinsic.bin");
       const decoded = codec_Et.decode(bin).value;
       expect(codec_Et.encodedSize(decoded)).toBe(bin.length);
       const reencoded = encodeWithCodec(codec_Et, decoded);
       expect(Buffer.from(reencoded).toString("hex")).toBe(
         Buffer.from(bin).toString("hex"),
       );
+    });
+    it("tickets_extrinsic.json", () => {
+      const json = JSON.parse(
+        Buffer.from(getCodecFixtureFile("tickets_extrinsic.json")).toString(
+          "utf8",
+        ),
+      );
+      const decoded = codec_Et_JSON.fromJSON(json);
+      const reencoded = codec_Et_JSON.toJSON(decoded);
+      expect(reencoded).deep.eq(json);
     });
   });
 }
