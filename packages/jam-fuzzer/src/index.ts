c594408ab01ba133eea1c7fa7f2fea2a64692f4f
Andrea Baccega <vekexasia@gmail.com>
2025-08-26 22:19:35 +0200

    fuzzer can now replicate traces

--- packages/jam-fuzzer/src/index.ts
+++ packages/jam-fuzzer/src/index.ts
@@ -13,23 +13,32 @@ import net from "net";
 import { BufferJSONCodec, E_4_int, encodeWithCodec } from "@tsjam/codec";
 import { EPOCH_LENGTH } from "@tsjam/constants";
 import {
+  IdentityMap,
   JamBlockImpl,
   JamStateImpl,
   merkleStateMap,
   SlotImpl,
+  stateFromMerkleMap,
   TauImpl,
 } from "@tsjam/core";
-import { Posterior, u32, u8, Validated } from "@tsjam/types";
+import {
+  HeaderHash,
+  Posterior,
+  StateKey,
+  StateRootHash,
+  u32,
+  u8,
+  Validated,
+} from "@tsjam/types";
 import packageJSON from "../package.json";
 import { produceBlock } from "./block-generator";
 import { GENESIS, GENESIS_STATE } from "./genesis";
+import fs from "fs";
+
+import path from "path";
+import { loadTrace, TraceState } from "./trace-stuff";
+import assert from "assert";
 const SOCKET_PATH = "/tmp/jam_target.sock";
-
-let state: JamStateImpl = GENESIS_STATE;
-state = GENESIS_STATE;
-let lastBlock: JamBlockImpl = GENESIS;
-lastBlock = GENESIS;
-
 const sendStuff = (
   message: Message,
   responseType: MessageType,
@@ -61,12 +70,14 @@ const sendStuff = (
   });
 };
 
-const compareState = async () => {
-  const merkleMap = merkleStateMap(state);
+const compareState = async (
+  merkleMap: IdentityMap<StateKey, 31, Uint8Array>,
+  lastHeaderHash: HeaderHash,
+) => {
   // request full state
   const fullState = await sendStuff(
     new Message({
-      getState: new GetState({ headerHash: lastBlock.header.signedHash() }),
+      getState: new GetState({ headerHash: lastHeaderHash }),
     }),
     MessageType.STATE,
   );
@@ -132,6 +143,33 @@ const client = net.createConnection({ path: SOCKET_PATH }, async () => {
     MessageType.PEER_INFO,
   );
   console.log(`Server Peer: `, peerResponse.peerInfo!.toJSON());
+  await sendSingleBlockFromTrace();
+  // await generateBlocks();
+});
+
+const checkState = async (
+  state: { new: TraceState; prev: TraceState },
+  lastHeaderHash: HeaderHash,
+  responseStateRoot: StateRootHash,
+) => {
+  if (Buffer.compare(state.new.stateRoot, responseStateRoot) !== 0) {
+    console.log("State roots differ");
+    if (Buffer.compare(state.prev.stateRoot, responseStateRoot) === 0) {
+      console.log("But previous state root matches");
+      console.log("Block was not applied from target");
+    } else {
+      await compareState(state.new.merkleMap, lastHeaderHash);
+    }
+    return false;
+  }
+  return true;
+};
+
+const generateBlocks = async () => {
+  let state: JamStateImpl = GENESIS_STATE;
+  state = GENESIS_STATE;
+  let lastBlock: JamBlockImpl = GENESIS;
+  lastBlock = GENESIS;
 
   const merkleMap = merkleStateMap(state);
   // set genesis state
@@ -146,7 +184,10 @@ const client = net.createConnection({ path: SOCKET_PATH }, async () => {
   );
 
   if (Buffer.compare(state.merkleRoot(), stateRootResponse.stateRoot!) !== 0) {
-    return await compareState();
+    return await compareState(
+      merkleStateMap(state),
+      lastBlock.header.signedHash(),
+    );
   }
 
   for (let i = EPOCH_LENGTH + 1; ; i++) {
@@ -158,9 +199,82 @@ const client = net.createConnection({ path: SOCKET_PATH }, async () => {
       new Message({ importBlock: newState.block! }),
       MessageType.STATE_ROOT,
     );
-    if (Buffer.compare(newState.merkleRoot(), res.stateRoot!) !== 0) {
-      return await compareState();
+    const isSuccess = await checkState(
+      {
+        new: new TraceState({
+          stateRoot: newState.merkleRoot(),
+          merkleMap: merkleStateMap(newState),
+        }),
+        prev: new TraceState({
+          stateRoot: state.merkleRoot(),
+          merkleMap: merkleStateMap(state),
+        }),
+      },
+      newState.block!.header.signedHash(),
+      res.stateRoot!,
+    );
+    if (!isSuccess) {
+      return;
     }
     state = newState;
+    lastBlock = newState.block!;
   }
-});
+};
+
+const sendSingleBlockFromTrace = async () => {
+  assert(process.env.TRACE_PATH, "TRACE_PATH environment variable is not set");
+  assert(
+    fs.existsSync(process.env.TRACE_PATH),
+    `TRACE_PATH ${process.env.TRACE_PATH} does not exist`,
+  );
+  console.log(`Loading trace from ${process.env.TRACE_PATH}`);
+  const trace = loadTrace(fs.readFileSync(process.env.TRACE_PATH, "utf8"));
+  const dir = path.dirname(process.env.TRACE_PATH!);
+  const files = fs.readdirSync(dir).filter((a) => a.endsWith(".json"));
+  const index = files.findIndex(
+    (a) => path.basename(a) === path.basename(process.env.TRACE_PATH!),
+  );
+
+  console.log(
+    `Loading prevTrace from ${path.join(dir, files[index - 1])} (index ${index})`,
+  );
+  const prevTrace = loadTrace(
+    fs.readFileSync(path.join(dir, files[index - 1]), "utf8"),
+  );
+  const sr = await sendStuff(
+    new Message({
+      setState: new SetState({
+        header: prevTrace.block.header,
+        state: new State({ value: trace.preState.merkleMap }),
+      }),
+    }),
+    MessageType.STATE_ROOT,
+  );
+
+  console.log("Checking pre-state");
+  console.log(BufferJSONCodec().toJSON(trace.preState.stateRoot));
+  console.log(TraceState.codecOf("merkleMap").toJSON(trace.preState.merkleMap));
+  if (Buffer.compare(trace.preState.stateRoot, sr.stateRoot!) !== 0) {
+    return await compareState(
+      trace.preState.merkleMap,
+      prevTrace.block.header.signedHash(),
+    );
+  }
+  console.log("Pre-state good, sending block");
+
+  const res = await sendStuff(
+    new Message({ importBlock: trace.block }),
+    MessageType.STATE_ROOT,
+  );
+
+  const isSuccess = await checkState(
+    { new: trace.postState, prev: trace.preState },
+    prevTrace.block.header.signedHash(),
+    res.stateRoot!,
+  );
+  if (!isSuccess) {
+    return;
+  }
+
+  console.log("All good");
+};
---
+++ packages/jam-fuzzer/src/trace-stuff.ts
@@ -0,0 +1,62 @@
+import {
+  BaseJamCodecable,
+  codec,
+  JamCodecable,
+  LengthDiscrimantedIdentityCodec,
+  xBytesCodec,
+} from "@tsjam/codec";
+import {
+  IdentityMap,
+  IdentityMapCodec,
+  JamBlockImpl,
+  JamSignedHeaderImpl,
+} from "@tsjam/core";
+import type { StateKey, StateRootHash } from "@tsjam/types";
+
+@JamCodecable()
+export class TraceState extends BaseJamCodecable {
+  @codec(xBytesCodec(32), "state_root")
+  stateRoot!: StateRootHash;
+
+  @codec(
+    IdentityMapCodec(xBytesCodec(31), LengthDiscrimantedIdentityCodec, {
+      key: "key",
+      value: "value",
+    }),
+    "keyvals",
+  )
+  merkleMap!: IdentityMap<StateKey, 31, Uint8Array>;
+
+  constructor(config?: Pick<TraceState, "merkleMap" | "stateRoot">) {
+    super();
+
+    if (typeof config !== "undefined") {
+      this.merkleMap = config.merkleMap;
+      this.stateRoot = config.stateRoot;
+    }
+  }
+}
+
+@JamCodecable()
+export class TraceStep extends BaseJamCodecable {
+  @codec(TraceState, "pre_state")
+  preState!: TraceState;
+
+  @codec(JamBlockImpl)
+  block!: JamBlockImpl;
+
+  @codec(TraceState, "post_state")
+  postState!: TraceState;
+}
+
+@JamCodecable()
+export class GenesisTrace extends BaseJamCodecable {
+  @codec(JamSignedHeaderImpl)
+  header!: JamSignedHeaderImpl;
+  @codec(TraceState)
+  state!: TraceState;
+}
+
+export const loadTrace = (jsonStr: string) => {
+  return TraceStep.fromJSON(JSON.parse(jsonStr));
+};
